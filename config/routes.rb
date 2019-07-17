25cbcfa21dc53a3fa097bf672f5096481ca08787
David James Baines <d.baines@guihenjones.com.au>
2019-07-17 12:03:16 +1000

    The past 100 changes

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+    resources :seats
+    resources :showtimes
+    resources :cinemas
+    resources :movies
+    resources :classifications
+    resources :genres
+    devise_for :users
+
+    # httpverb "url", to:"controller#action", as: "prefix"
+    delete "/artwork/:id", to: "artwork#destroy", as: "artwork_delete"
+
+    root to: 'movies#index'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
